2a80129ba94d90107574977be2804dab9781bca9
Cuong Nguyen <99136398+NMCxyz@users.noreply.github.com>
2023-04-16 23:39:55 +0700

    Add files via upload

--- Lab1/SecondDegreeEquationSolver.java
+++ Lab1/SecondDegreeEquationSolver.java
@@ -1,30 +1,34 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
-public class SecondDegreeEquationSolver {
-    public static void main(String[] args) {
-        // Get input values from user using JOptionPane
-        String aString = JOptionPane.showInputDialog("Enter the coefficient of x^2:");
-        String bString = JOptionPane.showInputDialog("Enter the coefficient of x:");
-        String cString = JOptionPane.showInputDialog("Enter the constant:");
+public class SecondDegreeEquation {
+   public static void main(String[] args) {
 
-        // Convert input strings to double values
-        double a = Double.parseDouble(aString);
-        double b = Double.parseDouble(bString);
-        double c = Double.parseDouble(cString);
+      double a, b, c, discriminant, root1, root2;
 
-        // Calculate the discriminant
-        double discriminant = b * b - 4 * a * c;
+      Scanner input = new Scanner(System.in);
 
-        // Check if the discriminant is negative
-        if (discriminant < 0) {
-            JOptionPane.showMessageDialog(null, "The equation has no real roots.");
-        } else {
-            // Calculate the two roots
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      System.out.println("Enter the value of a: ");
+      a = input.nextDouble();
 
-            // Display the roots using JOptionPane
-            JOptionPane.showMessageDialog(null, "The roots are " + root1 + " and " + root2);
-        }
-    }
+      System.out.println("Enter the value of b: ");
+      b = input.nextDouble();
+
+      System.out.println("Enter the value of c: ");
+      c = input.nextDouble();
+
+      discriminant = b * b - 4 * a * c;
+
+      if (discriminant > 0) {
+         root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+         root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+         System.out.println("The roots are: " + root1 + " and " + root2);
+      } else if (discriminant == 0) {
+         root1 = -b / (2 * a);
+
+         System.out.println("The root is: " + root1);
+      } else {
+         System.out.println("The equation has no real roots.");
+      }
+   }
 }
